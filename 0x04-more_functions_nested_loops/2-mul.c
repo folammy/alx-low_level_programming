02d8ee0b47b0c1aaacc5fe2b7b4e686be54fd7dc
folammy <oluwabamisefola217@gmail.com>
2022-12-15 18:41:14 -0800

    More functions nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+ * mul - multiples two integers
+ *
+ * @a:  first input
+ * @b: second input
+ *
+ * Return: return results
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * print_numbers - print 0 - 9
+ *             only using _putchar twice
+ *
+ * Return: Always return 0
+ */
+
+void print_numbers(void)
+{
+	int num = 0;
+
+	do {
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
+	_putchar('\n');
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact 
