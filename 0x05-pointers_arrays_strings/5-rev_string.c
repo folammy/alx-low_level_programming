2710fbf336eb8542a279a8a06a30d173544dee62
folammy <oluwabamisefola217@gmail.com>
2022-12-19 17:58:36 -0800

    Pointers arrays strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include"main.h"
+
+/**
+ * rev_string - reverse a string
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
+ */
+
+void rev_string(char *s)
+{
+	int i = 0, a = 0, Aux;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (i > a)
+	{
+		Aux = s[i];
+		s[i--] = s[a];
+		s[a++] = Aux;
+	}
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,11 @@
+Resources
+
+Read or watch:
+
+C - Arrays
+
+C - Pointers
+
+C - Strings
+
+Memory Layout
